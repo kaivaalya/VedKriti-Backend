173809f7c2ff69eaff60a3458a05dec42556e377
aaryan0804 <aaryan.ch.08@gmail.com>
2026-07-21 18:19:39 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const cookieParser=require("cookie-parser");
 const authRouter = require("./routes/auth.routes");
+const adminRouter = require("./routes/admin.routes");
 
 const app=express()
 
 
 app.use(cookieParser());
 app.use(express.json())
+app.use("/api/admin", adminRouter);
 
 
 
