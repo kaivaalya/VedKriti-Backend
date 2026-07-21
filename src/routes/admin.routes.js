cf6410afed0af4a1fa54fb6e8bd3bb8726bed84d
aaryan0804 <aaryan.ch.08@gmail.com>
2026-07-21 18:17:11 +0530

    Create admin.routes.js

---
+++ src/routes/admin.routes.js
@@ -0,0 +1,42 @@
+const express=require("express");
+
+const router=express.Router();
+
+const adminController=require("../controllers/admin.controller");
+const {
+    getPlatformStats
+} = require("../controllers/admin.controller");
+const {
+    getPendingDoctors
+} = require("../controllers/admin.controller");
+const {
+    verifyDoctor
+} = require("../controllers/admin.controller");
+const {
+    getAllDoctors
+} = require("../controllers/admin.controller");
+const {
+    removeDoctor
+}=require("../controllers/admin.controller")
+const {
+    adminLogin,
+    verify,
+    refreshToken,clearCookie
+} = require("../controllers/admin.controller");
+
+
+const { protect } = require("../middlewares/auth.middleware");
+
+
+router.post("/login", adminLogin);
+
+router.get("/verify", protect, verify);
+router.post("/refresh-token", refreshToken);
+
+router.get("/dashboard", getPlatformStats);
+router.get("/doctors/pending", getPendingDoctors);
+router.patch("/doctors/:id/verify", verifyDoctor);
+router.get("/doctors", getAllDoctors);
+router.delete("/doctors/:id", removeDoctor);
+
+module.exports=router;
