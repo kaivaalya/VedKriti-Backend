93724c572e03cf2020a25f822c8bd76a4246905d
Kaivalya Landge <kaivalyalandge2@gmail.com>
2026-08-01 14:38:34 +0530

    Update auth.middleware.js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,22 +6,27 @@ const Doctor = require("../models/Doctor.model");
 
 const protect = (req, res, next) => {
     try {
-        console.log("Protect started");
-
         const authHeader = req.headers.authorization;
-        console.log(authHeader);
+
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            throw new AppError("No token provided. Please log in.", 401);
+        }
 
         const accessToken = authHeader.split(" ")[1];
-
         const decoded = verifyAccessToken(accessToken);
-        console.log(decoded);
 
         req.user = decoded;
 
-        console.log("Protect finished");
         next();
     } catch (err) {
-        console.log(err);
+        if (err.name === "TokenExpiredError") {
+            return next(new AppError("Access token expired.", 401));
+        }
+
+        if (err.name === "JsonWebTokenError") {
+            return next(new AppError("Invalid access token.", 401));
+        }
+
         next(err);
     }
 };
