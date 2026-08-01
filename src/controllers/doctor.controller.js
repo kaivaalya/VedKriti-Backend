6b2930dfcdf56d7c5ff9bf2a00ef97604f9b0e69
Kaivalya Landge <kaivalyalandge2@gmail.com>
2026-08-01 20:00:30 +0530

    Update doctor.controller.js

--- src/controllers/doctor.controller.js
+++ src/controllers/doctor.controller.js
@@ -163,7 +163,7 @@ exports.getOperationalDetails = async (req,res,next)=>{
 exports.setAbout = async (req,res,next)=>{
     try{
 
-        const {destination,about}=req.body;
+        const {designation,description}=req.body;
         const docID = req.user.id;
         const updateData = {designation,about};
 
