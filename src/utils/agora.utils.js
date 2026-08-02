1a7b845fc01af17cad0c910e29317629334b7ead
Kaivalya Landge <kaivalyalandge2@gmail.com>
2026-08-02 13:48:13 +0530

    Update agora.utils.js

--- src/utils/agora.utils.js
+++ src/utils/agora.utils.js
@@ -1,23 +1,15 @@
-const { RtcTokenBuilder, RtcRole } = require("agora-token");
+const { RtcTokenBuilder, RtcRole } = require('agora-token');
 
-console.log("RtcRole:", RtcRole);
-console.log("RtcTokenBuilder:", RtcTokenBuilder);
 
 const generateAgoraToken = (channelName, uid, expirySeconds = 3600) => {
-  const appId = process.env.AGORA_APP_ID;
+  const appId          = process.env.AGORA_APP_ID;
   const appCertificate = process.env.AGORA_APP_CERTIFICATE;
 
-  console.log({
-    appId,
-    appCertificate,
-    appIdLength: appId?.length,
-    appCertificateLength: appCertificate?.length,
-    channelName,
-    uid,
-    role: RtcRole.PUBLISHER,
-  });
+  if (!appId || !appCertificate) {
+    throw new Error('AGORA_APP_ID and AGORA_APP_CERTIFICATE must be set in environment variables.');
+  }
 
-  const currentTime = Math.floor(Date.now() / 1000);
+  const currentTime  = Math.floor(Date.now() / 1000);
   const privilegeExpireTime = currentTime + expirySeconds;
 
   const token = RtcTokenBuilder.buildTokenWithUid(
@@ -29,8 +21,6 @@ const generateAgoraToken = (channelName, uid, expirySeconds = 3600) => {
     privilegeExpireTime,
     privilegeExpireTime
   );
-
-  console.log("Generated Token:", token);
 
   return token;
 };
